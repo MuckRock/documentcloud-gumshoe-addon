1d0a3593a6ac6ee38079674c30ae62c51becae50
Mitchell Kotler <mitch@mitchellkotler.net>
2022-09-09 09:34:35 -0400

    pass the run id and wait for the progress to get to 100 before ending

--- main.py
+++ main.py
@@ -5,18 +5,34 @@ It runs the Gumshoe machine learning model on a collection of DocumentCloud
 documents
 """
 
+import time
+
 import requests
 from documentcloud.addon import AddOn
 
 URL = "https://us-central1-gumshoe-muckrock-0ce3.cloudfunctions.net/gumshoe3"
+POLL_INTERVAL = 120
 
 
 class Gumshoe(AddOn):
+
+    def get_progress(self):
+        """Get the progress for the current run."""
+        if not self.id:
+            return None
+        resp = self.client.get(f"addon_runs/{self.id}/")
+        if resp.status_code == 200:
+            return resp.json()["progress"]
+
+        return None
+
+
     def main(self):
         # fetch your add-on specific data
         relevant = self.data.get("relevant", "")
         irrelevant = self.data.get("irrelevant", "")
         email = self.data.get("email", "")
+        run_id = self.id
 
         document_ids = [d.id for d in self.get_documents()]
 
@@ -28,6 +44,7 @@ class Gumshoe(AddOn):
                 "relevant_keywords": relevant,
                 "irrelevant_keywords": irrelevant,
                 "email": email,
+                "run_id": run_id,
             },
         )
 
@@ -35,6 +52,11 @@ class Gumshoe(AddOn):
             self.set_message("Error submitting request")
 
         self.set_message("Request submitted, please wait for the request to process")
+        progress = 0
+        while progress != 100:
+            progress = self.get_progress()
+            print(progress)
+            time.sleep(POLL_INTERVAL)
 
 
 if __name__ == "__main__":
